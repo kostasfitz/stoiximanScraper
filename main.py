40db48708fadda0369af7323dd819fe6d9d14e31
kostasfitz <kostasfitzios@gmail.com>
2022-11-16 11:34:29 +0200

    0% winner bug needs to be fixed

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from tennis_scrape import tennis_scrape
 
 # scraper setup
 driver_path = 'C:\Drivers\chromedriver_win32\chromedriver.exe'
-website = 'https://en.stoiximan.gr/sport/tennis/leagues/189086r/'
+website = 'https://en.stoiximan.gr/sport/tenis/diorganoseis/199527r,199528r/'
 
 tennis_scrape(driver_path, website)
